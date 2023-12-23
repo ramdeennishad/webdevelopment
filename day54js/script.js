b2b379d284ae7bd056af59c8b23b2caaba36fa86
Ramdeen Nishad <ramdeennishadraj273209@gmail.com>
2023-12-23 17:50:23 +0530

    day 54 completed js start

---
+++ day54js/script.js
@@ -0,0 +1,16 @@
+alert("Hello World");
+console.log("hey i am aryan");
+console.log("hey i am aryan1");
+console.log("hey i am aryan2");
+
+var a=prompt("Enter your number")
+var isTrue=confirm("Are you sure want to leave this page and blastyour computer")
+if(isTrue){
+    console.log("computer is blasting")
+}else{
+    console.log("not blasting")
+}
+console.log("your number is:-" +a)
+
+document.title="hey I am title"
+document.body.style.backgroundColor="red"
