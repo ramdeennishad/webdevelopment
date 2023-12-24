d3c2f28ab484b55ead33f9a5644f98f7bc1a937b
Ramdeen Nishad <ramdeennishadraj273209@gmail.com>
2023-12-24 23:55:00 +0530

    day67 completed

---
+++ day65/index.js
@@ -0,0 +1,2 @@
+// Exercise 11 to find factorial of a number
+// using reduce and using for loop
---
+++ day67/script.js
@@ -0,0 +1,5 @@
+console.log("heloo world")
+document.body.firstElementChild
+document.body.firstElementChild.childNodes
+document.body.firstElementChild.children
+
