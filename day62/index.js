c0396df6ab3fbacda7c6f377130dff5414ce871d
Ramdeen Nishad <ramdeennishadraj273209@gmail.com>
2023-12-24 15:04:13 +0530

    day62 completd

---
+++ day62/index.js
@@ -0,0 +1,17 @@
+// Exercise-Business Name Generator
+// Write a program that generates business names using the following rules:
+// by combining list of adjective and shop names and another word
+//adjective:
+// crazy
+// amazing 
+// Fire
+
+// shop name:
+// Engine
+// foods
+// Garments
+
+// Another word:
+// Bros
+// Limited
+// Hub
