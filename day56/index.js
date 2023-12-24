4a9b98ffb6b82e1b86cfb358664f8ba151b12a6e
Ramdeen Nishad <ramdeennishadraj273209@gmail.com>
2023-12-24 10:48:20 +0530

    day 56 completd

---
+++ day56/index.js
@@ -0,0 +1,14 @@
+console.log(" I am conditional statement")
+
+let a =45;
+let grace =2;
+console.log(a+grace)
+
+if(a+grace>18){
+    console.log("you can drive")
+}
+else{
+    console.log("you are not allowed to drive");
+}
+
+let  c=a>b ?(a-b):(a+b);
