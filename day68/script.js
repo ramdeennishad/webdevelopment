32fe284d34bfaafedaf4e30038ee64b0144a8ff6
Ramdeen Nishad <ramdeennishadraj273209@gmail.com>
2023-12-25 12:51:11 +0530

    day 68 completed

---
+++ day68/script.js
@@ -0,0 +1,16 @@
+console.log("aryan")
+// let boxes= document.getElementsByClassName("box")
+// console.log(boxes)
+
+// boxes[2].style.backgroundColor ="red"
+
+// document.getElementById("red").style.backgroundColor="red"
+//document.querySelector(".box").style.backgroundColor="green"
+
+// console.log(document.querySelectorAll(".box"))
+// document.querySelectorAll(".box").forEach( e => {
+//     //console.log(e)
+//     e.style.backgroundColor="green"
+// })
+
+// document.getElementsByTagName("div")
