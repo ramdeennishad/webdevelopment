115f97bef084ce7ccb5c43c74cb920b1e69171fd
Ramdeen Nishad <ramdeennishadraj273209@gmail.com>
2024-04-17 21:16:08 +0530

    day 85 completed

---
+++ day85/mymodule.js
@@ -0,0 +1,18 @@
+export const a=1
+export const b=2
+export const c=3
+export const d=4
+export const e=5
+// !st method
+//let obj;
+// export default obj={
+//     x:5,
+//     y:7
+// }
+
+const obj= {
+    x:5,
+    y:7
+}
+
+export default obj; //default export
