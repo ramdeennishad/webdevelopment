128d74265fee0afb0f37755ea0f5a7537ebd93d7
Ramdeen Nishad <ramdeennishadraj273209@gmail.com>
2024-04-22 00:00:48 +0530

    day89 completed

---
+++ day89/agenda.md
@@ -0,0 +1,5 @@
+1.Handling post and other request
+2. Chaining of requests
+3. Serving of html files
+4. Installing Postman
+5. Express router
---
+++ day89/main.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+const blog = require('./routes/blog')
+
+
+app.use(express.static("public"));
+app.use('/blog', blog)
+
+app
+  .get("/", (req, res) => {
+    console.log("hey its get request");
+
+    res.send("Hello World2244");
+  })
+  .post("/", (req, res) => {
+    console.log("this is post request");
+
+    res.send("Hello World by POST");
+  })
+  .put("/", (req, res) => {
+    console.log("this is put request");
+
+    res.send("Hello World by PUT");
+  });
+
+app.get("/index", (req, res) => {
+  console.log("this is index ");
+
+  res.sendFile("templates/index.html", { root: __dirname });
+});
+
+app.get("/api", (req, res) => {
+  console.log("this is index ");
+
+  res.json({ a: 1, b: 2, c: 3, d: 4 , name:["ram","shyam"]});
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
