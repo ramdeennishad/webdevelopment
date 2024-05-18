0aae698261f2be3be9270454a85c5f65243a256f
Ramdeen Nishad <ramdeennishadraj273209@gmail.com>
2024-05-18 18:55:06 +0530

    day91 completed

---
+++ day/index.js
@@ -0,0 +1,15 @@
+// you have to write a node js program to clear clutter inside of a directry and oraganize the content of that directry in to different folders.
+
+// for example, these files become:
+// 1. name.jpg
+// 2. name.png
+// 3. this.pdf
+// 4. rn.zip
+// 5. rohan.zip
+// 6. cat.jpg
+
+// this:
+// jpg/name.jpg, jpg/cat.jpg
+// png/name.png
+// zip/rn.zip zip/RTCDTMFToneChangeEvent.zip
+// pdf/this.pdf
